698889a2ca1327aca61a0db06cd354ebfe7a8c63
Konstantin <dr.kon5151@yandex.ru>
2018-05-27 19:17:15 +0300

    final version

--- threads-rdx-sort/rdx-sort.cpp
+++ threads-rdx-sort/rdx-sort.cpp
@@ -1,12 +1,14 @@
 #include <time.h>
 #include <omp.h>
 
+#include <cstring>
 #include <vector>
 #include <thread>
 #include <iostream>
 using namespace std;
 
-void TestSorting(int *data1, int *data2, int n) {
+// проверка сортировки двух одинаковых массивов
+bool TestSorting(int *data1, int *data2, int n) {
 	bool flag = true;
 
 	for (int i = 0; i < n - 1; i++) {
@@ -20,11 +22,10 @@ void TestSorting(int *data1, int *data2, int n) {
 
 	if (data1[n - 1] != data2[n - 1]) flag = false;
 
-	if (flag == true)
-		cout << "correct" << endl;
-	else cout << "error" << endl;
+	return flag;
 }
 
+// подсчет значений каждого байта в массиве
 void CreateCounters(int *data, int *counters, int n) {
 	memset(counters, 0, 256 * sizeof(int) * sizeof(int));
 
@@ -38,6 +39,7 @@ void CreateCounters(int *data, int *counters, int n) {
 	}
 }
 
+// сортировка data по указанному байту
 void ByteSort(int *data, int *temp, int *counter, int byte, int n) {
 	unsigned char *byteP;
 	int sum, c;
@@ -49,11 +51,14 @@ void ByteSort(int *data, int *temp, int *counter, int byte, int n) {
 		sum += c;
 	}
 
+	// при втором проходе по исходному массиву выполняется копирование элемента
+	// во вспомогательный массив по соответствующему индексу в массиве подсчётов
 	byteP = (unsigned char *)data + byte;
 	for (int i = 0; i < n; i++, byteP += sizeof(int))
 		temp[counter[*byteP]++] = data[i];
 }
 
+// поразрядная сортировка массива data размера n.
 void radixSort(int* data, int n) {
 	int *temp = new int[n];
 	int *counters = new int[sizeof(int) * 256];
@@ -71,7 +76,8 @@ void radixSort(int* data, int n) {
 	delete[] counters;
 }
 
-void omp_radixSort(int* data, int n, int nThreads) {
+// поразрядная сортировка с использованием OpenMP
+void radixSort_omp(int* data, int n, int nThreads) {
 	int *temp = new int[n];
 	unsigned char *byteP = (unsigned char*)data;
 	int byte = 0;
@@ -89,6 +95,8 @@ void omp_radixSort(int* data, int n, int nThreads) {
 			int tid = omp_get_thread_num();
 			threadCounter = counters + 256 * tid;
 
+// каждый поток получает на обработку часть массива
+// и выполняет подсчет элементов в свой массив подсчетов threadCounter.
 #pragma omp for
 			for (int i = 0; i < n; i++) {
 				byteP = (unsigned char*)data + i*sizeof(int) + byte;
@@ -96,6 +104,8 @@ void omp_radixSort(int* data, int n, int nThreads) {
 			}
 		}
 
+		// c помощью массивов подсчетов со всех потоков выполняется вычисление смещений,
+		// по которым будут располагаться элементы при втором проходе по массиву.
 		for (int k = 0; k < nThreads; k++) {
 			threadCounter = counters + 256 * k;
 			for (int i = 0; i < 256; i++) {
@@ -105,7 +115,7 @@ void omp_radixSort(int* data, int n, int nThreads) {
 
 		for (int i = 1; i < 256; i++) {
 			counter[i] += counter[i - 1];
-		}	
+		}
 
 		for (int i = nThreads - 1; i >= 0; i--) {
 			threadCounter = counters + 256 * i;
@@ -120,14 +130,17 @@ void omp_radixSort(int* data, int n, int nThreads) {
 			int tid = omp_get_thread_num();
 			threadCounter = counters + 256 * tid;
 
-#pragma omp for 
+// каждый поток получает на обработку ту же часть массива, что и ранее,
+// и выполняет копирование элемента во вспомогательный массив
+// по соответствующему индексу в массиве смещений.
+#pragma omp for
 			for (int i = 0; i < n; i++) {
 				byteP = (unsigned char*)data + i * sizeof(int) + byte;
 				temp[threadCounter[*byteP]++] = data[i];
 			}
 		}
 
-		swap(data, temp);	
+		swap(data, temp);
 	}
 
 	delete[] temp;
@@ -154,9 +167,12 @@ void Arrangement(int *data, int *temp, int *threadCounter, int n, int byte) {
 	}
 }
 
-void threads_radixSort(int* data, int n, int nThreads) {
+// реализация параллельной поразрядной сортировки
+// с использованием потоков C++ 11
+// (повторяет реализацию с использованием OpenMP)
+void radixSort_threads(int* data, int n, int nThreads) {
 	int *temp = new int[n];
-	int m = n / nThreads; 
+	int m = n / nThreads;
 	std::vector<std::thread> thr;
 	int byte = 0;
 
@@ -176,7 +192,7 @@ void threads_radixSort(int* data, int n, int nThreads) {
 		threadCounter = counters + 256 * (nThreads - 1);
 		CreateCounter(data + (nThreads - 1)*m, threadCounter, m, byte);
 		for (auto& t : thr) t.join();
-		thr.clear(); 
+		thr.clear();
 
 		for (int i = 0; i < nThreads; i++) {
 			threadCounter = counters + 256 * i;
@@ -185,10 +201,10 @@ void threads_radixSort(int* data, int n, int nThreads) {
 
 		for (int i = 1; i < 256; i++) {
 			counter[i] += counter[i - 1];
-		} 
+		}
 
 		for (int i = nThreads - 1; i >= 0; i--) {
-			threadCounter = counters + 256 * i; 
+			threadCounter = counters + 256 * i;
 			for (int k = 0; k < 256; k++) {
 				counter[k] -= threadCounter[k];
 				threadCounter[k] = counter[k];
@@ -205,20 +221,22 @@ void threads_radixSort(int* data, int n, int nThreads) {
 		for (auto& t : thr) t.join();
 		thr.clear();
 
-		swap(data, temp);	
+		swap(data, temp);
 	}
-	
+
 	delete[] temp;
 	delete[] counters;
 	delete[] counter;
 }
 
+
 int main(int argc, char* argv[]) {
-	double t1, t2;
 	int n = atoi(argv[1]);
 	int nThreads = atoi(argv[2]);
 	int *data1 = new int[n];
 	int *data2 = new int[n];
+
+	double t1, t2;
 
 	srand((unsigned)time(0));
 	for (int i = 0; i < n; i++) {
@@ -228,15 +246,18 @@ int main(int argc, char* argv[]) {
 
 	omp_set_num_threads(nThreads);
 	t1 = omp_get_wtime();
-	omp_radixSort(data1, n, nThreads);
+	radixSort_omp(data1, n, nThreads);
 	t2 = omp_get_wtime();
-	cout << "omp time = " << t2 - t1 << endl;
+	cout << "wall clock time (omp) = " << t2 - t1 << endl;
 
 	t1 = omp_get_wtime();
-	threads_radixSort(data2, n, nThreads);
+	radixSort_threads(data2, n, nThreads);
 	t2 = omp_get_wtime();
-	cout << "threads time = " << t2 - t1 << endl;
-	TestSorting(data1, data2, n);
+	cout << "wall clock time (threads) = " << t2 - t1 << endl;
+
+	// проверка
+	if (TestSorting(data1, data2, n)) printf("\ncorrect: data size = %d, # of threads = %d\n", n, omp_get_max_threads());
+    else printf("\nerror: data size = %d, # of threads = %d\n", n, omp_get_max_threads());
 
 	delete[] data1;
 	delete[] data2;
